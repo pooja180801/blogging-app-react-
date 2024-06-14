aae450b66d84f414e95048c859f87434ade4951d
poojak <139669968+pooja180801@users.noreply.github.com>
2024-06-14 17:08:45 +0530

    updated

--- src/PostPage.js
+++ src/PostPage.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-const PostPage = () => {
+const PostPage = ({posts,handleDelete}) => {
+  const {id}=useParams();
+  const post=posts.find(post=>(post.id).toString()===id);
   return (
-    <main>
+    <main className='postPage'>
+      <article className='post'>
+        {post &&
+        <>
+        <h2>{post.title}</h2>
+        <p className='postDate'>{post.datetime}</p>
+        <p className='postBody'>{post.body}</p>
+        <button onClick={()=>handleDelete(post.id)}>Delete Post</button>
+        </>
+        }
+
+        {!post &&
+
+      <main className='missing'>
+      <h2>Page not found</h2>
+      <p>Well,that's disappointing</p>
+      <p>Visit Our HomePage</p>
+      </main>
+        }
+
+      </article>
         
     </main>
   )
