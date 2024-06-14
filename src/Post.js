23de287d953abb8aa1e9b1a5b4497bfbb1dd4f41
poojak <139669968+pooja180801@users.noreply.github.com>
2024-06-14 17:08:39 +0530

    updated

--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Post = ({post}) => {
   return (
    <article className='post'>
+    <Link to={`post/${post.id}`} className='post-link'>
     <h2>{post.title}</h2>
     <p className='postDate'>{post.datetime}</p>
+    </Link>
     <p className='postBody'>{
         (post.body).length<=25 ?
         post.body : `${(post.body).slice(0,25)}....`
