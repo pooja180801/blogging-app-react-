866d0a8eb54b7e5dde284afa48d487439700210c
poojak <139669968+pooja180801@users.noreply.github.com>
2024-06-14 19:48:34 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -9,41 +9,36 @@ import NewPost from "./NewPost";
 import PostPage from "./PostPage";
 import { useEffect, useState } from "react";
 import { format } from 'date-fns';
-
+import api from './api/posts';
+import EditPost from "./EditPost";
 
 
 function App() {
   const navigate=useNavigate();
-  const [posts,setPosts]=useState([
-    {
-      id:1,
-      title:"my first post",
-      datetime:"Jun 13, 2024 6:36:44 PM",
-      body:"hello i completed my project today"
-    },
-    {
-      id:2,
-      title:"my first post",
-      datetime:"Jun 13, 2024 6:43:44 PM",
-      body:"hello i completed my project today"
-    },
-    {
-      id:3,
-      title:"my first post",
-      datetime:"Jun 13, 2024 6:23:44 PM",
-      body:"hello i completed my project today"
-    },
-    {
-      id:4,
-      title:"my first post",
-      datetime:"Jun 13, 2024 6:13:44 PM",
-      body:"hello i completed my project today"
-    }
-  ])
+  const [posts,setPosts]=useState([])
   const [search,setSearch]=useState('')
   const [searchResults,setSearchResults]=useState([])
   const [postTitle,setPostTitle]=useState('')
   const [postBody,setPostBody]=useState('')
+  const [editTitle,setEditTitle]=useState('')
+  const [editBody,setEditBody]=useState('')
+
+  useEffect(()=>{
+    const fetchProducts=async()=>{
+      try {
+        const response=await api.get('/posts')
+        setPosts(response.data)
+      } catch (error) {
+        if(error.response){
+        console.log(error.response.data)
+        console.log(error.response.status)
+        }else{
+          console.log(`Error: ${error.message}`)
+        }
+      }
+    }
+    fetchProducts();
+  },[])
 
   useEffect(()=>{
     const filteredResults=posts.filter((post)=>((post.body).toLowerCase()).includes(search.toLowerCase())
@@ -53,24 +48,60 @@ function App() {
   setSearchResults(filteredResults.reverse())
   },[posts,search])
 
-  const handleSubmit=(e)=>{
+  const handleSubmit=async(e)=>{
     e.preventDefault()
     const id=posts.length ? posts[posts.length-1].id + 1 : 1;
     const datetime=format(new Date(), 'MMM dd, yyyy pp');
     const newPost={id,title:postTitle,datetime,body:postBody}
-    const allPosts=[...posts,newPost]
+    try{
+    const response=await api.post('/posts',newPost)
+    const allPosts=[...posts,response.data]
     console.log(allPosts)
     setPosts(allPosts)
     setPostTitle('')
     setPostBody('')
     navigate('/')
+    }catch (error) {
+      if(error.response){
+      console.log(error.response.data)
+      console.log(error.response.status)
+      }else{
+        console.log(`Error: ${error.message}`)
+      }
+    }
   }
   
 
-  const handleDelete=(id)=>{
-    const postsList=posts.filter(post=>post.id!==id);
-    setPosts(postsList)
-    navigate('/')
+  const handleDelete = async (id) => {
+    try {
+        console.log("Deleting post with ID:", id);
+        await api.delete(`/posts/${id}`);
+        const postsList = posts.filter(post => post.id !== id);
+        setPosts(postsList);
+        navigate('/');
+    } catch (error) {
+      if(error.response){
+      console.log(error.response.data)
+      console.log(error.response.status)
+      }else{
+        console.log(`Error: ${error.message}`)
+      }
+    }
+};
+
+  const handleEdit=async(id)=>{
+    const datetime=format(new Date(), 'MMM dd, yyyy pp');
+    const updatedPost={id,title:editTitle,datetime,body:editBody}
+    try {
+      const response=await api.put('/posts/${id}',updatedPost)
+      setPosts(posts.map(post=>post.id===id ? {...response.data} : post))
+      setEditTitle('')
+      setEditBody('')
+      navigate('/')
+      
+    } catch (error) {
+      console.log(`Error: ${error.message}`)
+    }
 
   }
 
@@ -78,7 +109,7 @@ function App() {
     <div className="App">
   <Header title={"social-media-app"}/>
   <Nav search={search} setSearch={setSearch}/>
-  <Routes>
+  <Routes >
       <Route path="/" element={<Home posts={searchResults}/>}/>
       <Route path="post" >
       <Route index element={ <NewPost 
@@ -90,6 +121,16 @@ function App() {
       />
       <Route path=":id" element={<PostPage posts={posts} handleDelete={handleDelete}/> }/>
       </Route>
+
+      <Route path="/edit/:id" element={<EditPost 
+      posts={posts} 
+      handleEdit={handleEdit}
+      editBody={editBody} 
+      setEditBody={setEditBody} 
+      editTitle={editTitle} 
+      setEditTitle={setEditTitle}
+      />}/>
+
       <Route path="about" element={<About/>}/>
       <Route path="*" element={<Missing/>}/>
       </Routes>
