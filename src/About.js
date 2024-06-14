5bca48b842e6a1bdef1fed0cb723ccc83e6b6f10
poojak <139669968+pooja180801@users.noreply.github.com>
2024-06-14 16:22:29 +0530

    updated about.js

--- src/About.js
+++ src/About.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-  <main>
-    <h1>about</h1>
+  <main className='about'>
+    <h2 >About</h2>
+    <p>Welcome to our social media app!</p>
+      <p>This app allows you to create posts, search for content, and much more.</p>
+      <p>Explore and enjoy connecting with others!</p>
   </main>
   )
 }
