0070e35731b8f0d4f29da48ffb644cabf54b1399
poojak <139669968+pooja180801@users.noreply.github.com>
2024-06-15 15:07:25 +0530

    updated home

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Feed from './Feed'
+import DataContext from './context/DataContext'
 
-const Home = ({posts}) => {
+const Home = () => {
+  const {searchResults,fetchError,isLoading}=useContext(DataContext)
   return (
     <main className='Home'>
-        {posts.length ? (<Feed posts={posts}/>)
-        : (<p style={{marginTop:"2rem"}}>
+      {isLoading && <p className='statusMsg'>Loading Posts</p>}
+      {!isLoading && fetchError && <p className='statusMsg' style={{color:"red"}}>{fetchError}</p>}
+        {!isLoading && !fetchError && (searchResults.length ? (<Feed posts={searchResults}/>)
+        : <p style={{marginTop:"2rem"}}>
           No posts to display
         </p>)  
       }
