e36457dd312a8d1c79f5ef9597c3bc6f0d41755e
poojak <139669968+pooja180801@users.noreply.github.com>
2024-06-15 15:07:15 +0530

    updated header

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {FaLaptop,FaTabletAlt,FaMobileAlt} from 'react-icons/fa'
+import DataContext from './context/DataContext'
 
 const Header = ({title}) => {
+
+  const {width}=useContext(DataContext)
   return (
     <header className='header'>
         <h1>{title}</h1>
+        <div className='header-icon'>
+        {width<768 ? <FaMobileAlt/> 
+          : width < 992 ? <FaTabletAlt/>
+          : <FaLaptop/>
+
+        }
+        </div>
     </header>
   )
 }
